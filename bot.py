049ecf00398c87b994b89009a377c7a43dc81fdb
Muhammed Adnan Sami <34181354+adnansami1992sami@users.noreply.github.com>
2022-04-29 16:34:34 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,9 +32,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-client = pymongo.MongoClient(
-	
-	)# MONGO DB LINK 
+client = pymongo.MongoClient(mongodb+srv://adnan:<Allahone0@>@decode11telegram.tmbx8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority)# MONGO DB LINK 
 db = client.credit_cards
 
 developers = ['878216403']
